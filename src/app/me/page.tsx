c62dcd0d3a64bc1b486f19210757962c60bd59e8
Andcool-Systems <drolll2007@gmail.com>
2024-07-05 02:55:26 +0300

    fixed styles and tutorials

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -15,39 +15,7 @@ import { SkinViewer } from 'skinview3d';
 import { Card, generateSkin } from '@/app/modules/card.module';
 import { Me } from '@/app/modules/me.module';
 import Link from 'next/link';
-
-export const roles = [
-    {
-        id: 1142141232685006990,
-        title: "Игрок PepeLand 8",
-        color: 3003583
-    },
-    {
-        id: 1123363907692666910,
-        title: "Игрок экспериментов (до 8 сезона)",
-        color: 1357990
-    },
-    {   
-        id: 958432771519422476,
-        title: "Игрок PepeLand 7",
-        color: 3003583
-    },
-    {
-        id: 495989709265436687,
-        title: "Twitch Subs",
-        color: 6333946
-    },
-    {
-        id: 987234058478186506,
-        title: "Бусти",
-        color: 15105570
-    },
-    {
-        id: 589530176501579780,
-        title: "Discord Server Booster",
-        color: 15235577
-    }
-];
+import { roles } from '../roles';
 
 
 const Main = () => {
--- src/app/modules/info.module.tsx
+++ src/app/modules/info.module.tsx
@@ -8,7 +8,7 @@ interface InfoCardProps {
 
 
 const InfoCard = ({title, children, color}: InfoCardProps) => {
-    const _color = color || '#0000ff';
+    const _color = color || '#4493F8';
     return  (
             <div style={{borderColor: _color, background: `linear-gradient(90deg, ${_color} -10%, rgba(0,0,0,0) 40%)`}} className={Style.container}>
                 <h3 className={Style.header}>{title}</h3>

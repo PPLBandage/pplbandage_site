d1b341c7486e5bd1bc7a8c4ee4d39c2e36b911a9
AndcoolSystems <drolll2007@gmail.com>
2024-08-02 22:34:19 +0300

    added bandages link to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,12 @@
 import React from "react";
-import Header from "./modules/header.module";
-import Style from "./styles/root/page.module.css";
-import Image from "next/image";
-import Link from "next/link";
 import axios from "axios";
-import { CustomLink } from "./modules/search.module";
-import { Fira_Code } from "next/font/google";
-const fira = Fira_Code({ subsets: ["latin"] });
+import HomeClient from "./root_client";
 
 
 const Home = async () => {
     const pong = await axios.get(process.env.NEXT_PUBLIC_GLOBAL_API_URL + 'ping', { validateStatus: () => true });
-
-    let pepes: JSX.Element[] = [];
-    for (let i = 0; i < 10; i++) {
-        pepes.push(<img key={i} alt="" className={Style.img} src={`/static/pepes_main/${i}.png`} draggable={false} />)
-    }
     return (
-        <body style={{ overflow: "hidden", overflowY: "scroll" }}>
-            <Header />
-            <div className={Style.test}>
-                <svg width="958" height="318" className={Style.svg}>
-                    <path className={Style.path} d="M 477 159 C -159 -391 -159 709 477 159 C 1113 -391 1113 709 477 159" strokeWidth="3" strokeDasharray="10,10" stroke="rgba(45, 212, 191, .5)" fill="none" />
-                </svg>
-                <div className={Style.bandages}>
-                    {pepes}
-                </div>
-                <div className={Style.container}>
-                    <p className={Style.p}><span className={Style.one}>1</span> Сайт</p>
-                    <p className={Style.p} style={{ display: "flex", alignItems: "center" }}><Image src="/static/icons/infinity.svg" alt="inf" width={40} height={40} className={Style.inf} />Стилей</p>
-                    <Link href="/workshop" className={Style.link}>Открыть мастерскую</Link>
-                    {pong.status !== 200 &&
-                        <div className={Style.api_unavailable}>
-                            <div style={{ display: 'flex', alignItems: 'center', gap: '.5rem' }}>
-                                <Image src='/static/icons/blocks/warning.svg' alt="warning" width={24} height={24} />
-                                <h3 style={{ margin: 0 }}>Service Unavailable</h3>
-                            </div>
-                            <p>Сервис в настоящий момент недоступен. Попробуйте позже или обратитесь в <CustomLink href="/contacts">администрацию</CustomLink></p>
-                        </div>
-                    }
-                </div>
-            </div>
-        </body>
+        <HomeClient pong={pong.status} />
     )
 }
 
---
+++ src/app/root_client.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import Image from "next/image";
+import Header from "./modules/header.module";
+import React, { useState } from "react";
+import Style from "./styles/root/page.module.css";
+import Link from "next/link";
+import { CustomLink } from "./modules/search.module";
+
+
+const HomeClient = ({ pong }: { pong: number }) => {
+    const [animationState, setAnimationState] = useState<boolean>(true);
+    const pepes_links = ['ogukal', 'gauu3s', 'pe5s4d', 'x4rak9', '3t75jf', 'wrbs4h', 't6i5ld', 'gnikzr', 'by1lzs', '4psolk'];
+
+    let pepes: JSX.Element[] = [];
+    for (let i = 0; i < 10; i++) {
+        pepes.push(
+            <a key={i}
+                className={Style.img}
+                style={{ animationPlayState: animationState ? 'running' : 'paused' }}
+                href={`/workshop/${pepes_links[i]}`}>
+                <img alt=""
+                    src={`/static/pepes_main/${i}.png`}
+                    draggable={false}
+                    style={{ width: '60px' }}
+                    onMouseEnter={() => setAnimationState(false)}
+                    onMouseLeave={() => setAnimationState(true)} />
+            </a>
+        )
+    }
+    return (
+        <body style={{ overflow: "hidden", overflowY: "scroll" }}>
+            <Header />
+            <div className={Style.test}>
+                <svg width="958" height="318" className={Style.svg}>
+                    <path className={Style.path} d="M 477 159 C -159 -391 -159 709 477 159 C 1113 -391 1113 709 477 159" strokeWidth="3" strokeDasharray="10,10" stroke="rgba(45, 212, 191, .5)" fill="none" />
+                </svg>
+                <div className={Style.bandages}>
+                    {pepes}
+                </div>
+                <div className={Style.container}>
+                    <p className={Style.p}><span className={Style.one}>1</span> Сайт</p>
+                    <p className={Style.p} style={{ display: "flex", alignItems: "center" }}><Image src="/static/icons/infinity.svg" alt="" width={40} height={40} className={Style.inf} draggable={false} />Стилей</p>
+                    <Link href="/workshop" className={Style.link}>Открыть мастерскую</Link>
+                    {pong !== 200 &&
+                        <div className={Style.api_unavailable}>
+                            <div style={{ display: 'flex', alignItems: 'center', gap: '.5rem' }}>
+                                <Image src='/static/icons/blocks/warning.svg' alt="warning" width={24} height={24} />
+                                <h3 style={{ margin: 0 }}>Service Unavailable</h3>
+                            </div>
+                            <p>Сервис в настоящий момент недоступен. Попробуйте позже или обратитесь в <CustomLink href="/contacts">администрацию</CustomLink></p>
+                        </div>
+                    }
+                </div>
+            </div>
+        </body>
+    )
+}
+
+export default HomeClient;

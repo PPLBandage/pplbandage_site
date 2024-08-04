7a20a3e80dba99ce1e0f08126967b12bdba31531
AndcoolSystems <drolll2007@gmail.com>
2024-08-04 14:36:19 +0300

    fixed build

--- production_build/construct_env.py
+++ production_build/construct_env.py
@@ -1,15 +1,18 @@
 import os
 import datetime
 
-now = datetime.datetime.now()
+offset = datetime.timedelta(hours=3)
+now = datetime.datetime.now(datetime.timezone(offset, name='МСК'))
 formatted_date = now.strftime("%H:%M:%S %d.%m.%Y")
 
 with open('production_build/.env', 'w') as env:
-    env.write(f'NEXT_PUBLIC_API_URL={os.getenv('NEXT_PUBLIC_API_URL')}\n')
-    env.write(f'NEXT_PUBLIC_GLOBAL_API_URL={os.getenv('NEXT_PUBLIC_GLOBAL_API_URL')}\n')
-    env.write(f'NEXT_PUBLIC_COLORABLE_ID={os.getenv('NEXT_PUBLIC_COLORABLE_ID')}\n')
-    env.write(f'NEXT_PUBLIC_LOGIN_URL={os.getenv('NEXT_PUBLIC_LOGIN_URL')}\n')
-    env.write(f'TOKEN={os.getenv('TOKEN')}\n')
+    env.write(f'NEXT_PUBLIC_API_URL={os.getenv("NEXT_PUBLIC_API_URL")}\n')
+    env.write(
+        f'NEXT_PUBLIC_GLOBAL_API_URL={os.getenv("NEXT_PUBLIC_GLOBAL_API_URL")}\n')
+    env.write(
+        f'NEXT_PUBLIC_COLORABLE_ID={os.getenv("NEXT_PUBLIC_COLORABLE_ID")}\n')
+    env.write(f'NEXT_PUBLIC_LOGIN_URL={os.getenv("NEXT_PUBLIC_LOGIN_URL")}\n')
+    env.write(f'TOKEN={os.getenv("TOKEN")}\n')
     env.write(f'NEXT_PUBLIC_BUILD_DATE={formatted_date}\n')
 
 print('Successfully created .env file')

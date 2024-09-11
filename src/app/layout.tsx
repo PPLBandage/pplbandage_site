08be0a5e1abf3cc2ed14b943f120e4d14a683846
Andcool-Systems <drolll2007@gmail.com>
2024-09-12 00:03:39 +0300

    renamed meta.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,14 @@ import Providers from "@/app/modules/providers.module";
 import "./styles/layout.css";
 import { CookiesProvider } from 'next-client-cookies/server';
 import { headers } from 'next/headers';
-import OpenGraph from '@/app/openGraph.json';
+import meta from '@/app/meta.json';
 import { merge } from 'lodash';
 
 export const generateMetadata = async (): Promise<Metadata> => {
 	const headersList = headers();
 	const path = headersList.get('X-Forwarded-Path').split('?')[0];  // Working only with Nginx config!
-	const object = (OpenGraph as { [key: string]: any });
-	const base = OpenGraph.base;
+	const object = (meta as { [key: string]: any });
+	const base = meta.base;
 	const extend = object[path];
 	return merge({}, base, extend) as Metadata;
 }

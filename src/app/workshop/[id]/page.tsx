02081c71cbad6e6d0b7b9d4ad9227cc8e1300002
Andcool-Systems <drolll2007@gmail.com>
2024-09-11 21:27:40 +0300

    added dynamic metadata generating for bandage page

--- src/app/workshop/[id]/page.tsx
+++ src/app/workshop/[id]/page.tsx
@@ -3,9 +3,44 @@ import Home from "./client_code";
 import * as Interfaces from "@/app/interfaces";
 import { headers } from 'next/headers';
 import { notFound } from "next/navigation";
+import { Metadata } from "next";
+
+
+export const generateMetadata = async ({ params }: { params: { id: string } }): Promise<Metadata> => {
+    const meta = await axios.get(`${process.env.NEXT_PUBLIC_GLOBAL_API_URL}workshop/${params.id}/info`, {
+        validateStatus: () => true,
+        withCredentials: true,
+        headers: {
+            'Unique-Access': process.env.TOKEN,
+            'Cache-Control': 'no-cache',
+            'Pragma': 'no-cache',
+            'Expires': '0'
+        }
+    });
+
+    const data = meta.data.data as Interfaces.Bandage;
+    if (!data) return null;
+    return {
+        title: `${data.title} · Повязки Pepeland`,
+        description: data.description,
+        openGraph: {
+            title: `${data.title} · Повязки Pepeland`,
+            description: data.description,
+            url: `https://pplbandage.ru/workshop/${data.external_id}`,
+            siteName: 'Повязки Pepeland',
+            images: `https://pplbandage.ru/api/workshop/${data.external_id}/og`
+        },
+        twitter: {
+            card: 'summary_large_image'
+        },
+        other: {
+            'theme-color': data.average_og_color
+        }
+    }
+}
 
 const Main = async ({ params }: { params: { id: string } }) => {
-    const headersList = headers()
+    const headersList = headers();
     const cookie = headersList.get('Cookie');
     const userAgent = headersList.get('User-Agent');
 
@@ -26,24 +61,7 @@ const Main = async ({ params }: { params: { id: string } }) => {
     if (initial_response.status !== 200 || !data) {
         notFound();
     }
-    return (
-        <>
-            {data &&
-                <>
-                    <meta property="og:title" content={data.title} />
-                    <meta property="og:description" content={data.description || 'Повязки Пепеленда для всех! Хотите себе на скин модную повязку Pepeland? Тогда вам сюда!'} />
-                    <meta property="og:url" content={`https://pplbandage.ru/workshop/${data.external_id}`} />
-                    <meta property="og:site_name" content="Повязки Pepeland" />
-                    <meta property="og:image" content={`/api/workshop/${data.external_id}/og`} />
-                    <meta name="twitter:card" content="summary_large_image" />
-                    <meta name="theme-color" content={data.average_og_color} />
-                    <meta name="description" content={data.description || 'Повязки Пепеленда для всех! Хотите себе на скин модную повязку Pepeland? Тогда вам сюда!'} />
-                    <title>{`${data.title} · Повязки Pepeland`}</ title>
-                </>
-            }
-            <Home data={data} />
-        </>
-    )
+    return <Home data={data} />;
 }
 
 export default Main;

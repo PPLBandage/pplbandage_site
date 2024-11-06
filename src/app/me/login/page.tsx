814a99b74517ae1f0e29720e1950e795654a686c
Andcool-Systems <drolll2007@gmail.com>
2024-11-06 10:38:46 +0300

    changed local to global login url

--- src/app/me/login/page.tsx
+++ src/app/me/login/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from "next/navigation";
 
 
 const Login = async () => {
-    redirect(`${process.env.NEXT_PUBLIC_GLOBAL_API_URL}auth/url`);
+    redirect(`${process.env.NEXT_PUBLIC_API_URL}auth/url`);
 }
 
 export default Login;

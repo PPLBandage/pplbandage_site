26aaabe74f98c3bec0d4586bedcf0221362f717c
Andcool-Systems <drolll2007@gmail.com>
2024-11-06 10:34:20 +0300

    changed redirection method

--- src/app/me/login/page.tsx
+++ src/app/me/login/page.tsx
@@ -1,20 +1,8 @@
-import axios from "axios";
 import { redirect } from "next/navigation";
 
 
 const Login = async () => {
-    const response = await axios.get(`${process.env.NEXT_PUBLIC_GLOBAL_API_URL}auth/url`,
-        {
-            headers: {
-                'Accept': 'application/json',
-                'Cache-Control': 'no-cache',
-                'Pragma': 'no-cache',
-                'Expires': '0'
-            }
-        }
-    );
-    if (response.status !== 200) redirect('/me');
-    redirect(response.data.url);
+    redirect(`${process.env.NEXT_PUBLIC_GLOBAL_API_URL}auth/url`);
 }
 
 export default Login;

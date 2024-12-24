7e46ad4b392e88045922e284655590d747cb35d3
Andcool-Systems <drolll2007@gmail.com>
2024-12-24 22:39:43 +0300

    changed category icons

--- src/app/modules/utils/Categories.tsx
+++ src/app/modules/utils/Categories.tsx
@@ -1,12 +1,12 @@
 import {
-    IconBrush,
+    IconPuzzle,
     IconChristmasTree,
     IconClock,
     IconPalette,
     IconBan,
     IconFlag,
     IconPumpkinScary,
-    IconLeaf,
+    IconCalendar,
     IconSnowflake,
     IconSun,
     IconTree,
@@ -19,14 +19,14 @@ import {
 } from '@tabler/icons-react';
 
 const Icons: { [key: string]: JSX.Element } = {
-    'IconBrush': <IconBrush width={15} height={15} />,
+    'IconBrush': <IconPuzzle width={15} height={15} />,
     'IconChristmasTree': <IconChristmasTree width={15} height={15} />,
     'IconClock': <IconClock width={15} height={15} color="#ffd500" />,
     'IconPalette': <IconPalette width={15} height={15} />,
     'IconBan': <IconBan width={15} height={15} color='#ff2424' />,
     'IconFlag': <IconFlag width={15} height={15} />,
     'IconPumpkinScary': <IconPumpkinScary width={15} height={15} />,
-    'IconLeaf': <IconLeaf width={15} height={15} />,
+    'IconLeaf': <IconCalendar width={15} height={15} />,
     'IconSnowflake': <IconSnowflake width={15} height={15} />,
     'IconSun': <IconSun width={15} height={15} />,
     'IconTree': <IconTree width={15} height={15} />,

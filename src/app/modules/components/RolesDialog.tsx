81bc5ecb11cb6a2177ac47811b28989b95c60687
Andcool-Systems <drolll2007@gmail.com>
2025-01-06 16:29:43 +0300

    added roles dialog loading

--- src/app/modules/components/RolesDialog.tsx
+++ src/app/modules/components/RolesDialog.tsx
@@ -7,6 +7,9 @@ import ReactCSSTransition from "./CSSTransition";
 import { Role } from "@/app/interfaces";
 import ApiManager from "../utils/apiManager";
 import IconBoosty from '@/app/resources/boosty.svg';
+import IconPPL from '@/app/resources/icon.svg';
+import style_workshop from "@/app/styles/workshop/page.module.css";
+import IconSvg from '@/app/resources/icon.svg';
 
 
 const RolesDialog = ({ children }: { children: JSX.Element }) => {
@@ -59,13 +62,12 @@ const RolesDialog = ({ children }: { children: JSX.Element }) => {
                             <IconX className={style_base.close} onClick={() => setExpanded(false)} />
                         </div>
                         <p style={{ margin: 0, fontSize: '.9rem', opacity: .6 }}>Для регистрации Вы должны иметь одну из этих ролей на Discord сервере PWGood.</p>
-                        <div
-                            style={{
-                                display: 'flex',
-                                flexWrap: 'wrap'
-                            }}>
-                            {roles_el}
-                        </div>
+
+                        {
+                            roles_el.length !== 0 ?
+                                <div style={{ display: 'flex', flexWrap: 'wrap' }}>{roles_el}</div> :
+                                <IconSvg width={64} height={64} className={style_workshop.loading} />
+                        }
 
                         <h4 style={{ margin: 0, marginTop: '.5rem' }}>Ссылки на соцсети Пугода:</h4>
                         <div className={style_main.container}>
@@ -91,6 +93,15 @@ const RolesDialog = ({ children }: { children: JSX.Element }) => {
                                 } as CSSProperties}
                             >
                                 <IconBoosty />Boosty
+                            </a>
+                            <a
+                                className={style_main.link}
+                                href='https://pepeland.net'
+                                style={{
+                                    '--accent': '#0f766e'
+                                } as CSSProperties}
+                            >
+                                <IconPPL style={{ width: '24px', height: '24px' }} />Сайт Пепеленда
                             </a>
                         </div>
                     </div>

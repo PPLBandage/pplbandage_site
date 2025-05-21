3a7685155b366fc56e91ae7c945bb0d0029fdd8e
Andcool-Systems <drolll2007@gmail.com>
2025-05-21 23:05:54 +0300

    feat: added images domain

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "pplb.ru",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
   webpack(config) {

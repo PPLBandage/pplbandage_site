fa2ba4778310a0219cb5e317b45bceec19f8824e
Andcool-Systems <drolll2007@gmail.com>
2025-06-18 20:57:23 +0300

    feat: Added meta for not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,10 @@
-'use client';
-
 import NotFoundElement from '@/components/NotFound';
+import { merge } from 'lodash';
+import meta from '@/app/meta.json';
+
+export async function generateMetadata() {
+    return merge({}, meta.base, meta.notFound);
+}
 
 export default function NotFound() {
     return <NotFoundElement />;

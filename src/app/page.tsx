ca17e63f5f19f04c92bff1a5a8be92e6fa7c2724
Andcool-Systems <drolll2007@gmail.com>
2025-08-06 01:36:40 +0300

    feat: New main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,82 @@
-import React from 'react';
-import axios from 'axios';
-import HomeClient from './root_client';
+'use client';
 
-const Home = async () => {
-    let pong = null;
-    try {
-        pong = await axios.get(`${process.env.API_URL}/ping`, {
-            validateStatus: () => true
-        });
-    } catch {}
-    return <HomeClient pong={pong?.status} />;
+import React from 'react';
+import styles from '@/styles/root/page.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import IconCropped from '@/resources/icon-cropped.svg';
+import person from '@/resources/root/person.png';
+import bandage from '@/resources/root/bandage.png';
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
+
+const HomeClient = () => {
+    return (
+        <div>
+            <div className={styles.main_container}>
+                <div className={styles.left_shade} />
+                <div className={`${styles.left_shade} ${styles.right_shade}`} />
+                <div className={styles.container}>
+                    <div className={styles.texts}>
+                        <h1 className={`${styles.h1} ${GeistSans.className}`}>
+                            Огромная библиотека повязок{' '}
+                            <span className={styles.pepeland}>Пепеленда</span>
+                        </h1>
+                        <p className={styles.description}>
+                            Место, где любой найдет дизайн по вкусу.
+                        </p>
+                        <div className={styles.links}>
+                            <Link href="/workshop" className={styles.links_workshop}>
+                                Мастерская
+                            </Link>
+                            <Link href="/me">Личный кабинет</Link>
+                        </div>
+                        <p className={`${styles.by_andcool} ${GeistMono.className}`}>
+                            <IconCropped />
+                            {' by '}
+                            <Link href={'https://andcool.ru'}>AndcoolSystems</Link>
+                        </p>
+                    </div>
+                    <div className={styles.image_container}>
+                        <Image
+                            src={bandage}
+                            alt=""
+                            width={300}
+                            className={styles.image_bandage}
+                        />
+                        <Image
+                            src={person}
+                            alt=""
+                            width={300}
+                            className={styles.image_skin}
+                        />
+                        <svg
+                            width="250"
+                            height="100"
+                            xmlns="http://www.w3.org/2000/svg"
+                            className={styles.shadow}
+                        >
+                            <ellipse rx="125" ry="50" cx="125" cy="50" />
+                        </svg>
+                    </div>
+                </div>
+            </div>
+            <footer className={styles.footer}>
+                <p>
+                    Сайт pplbandage.ru не является официальной частью сети серверов{' '}
+                    <Link href="https://pepeland.net" target="_blank">
+                        PepeLand
+                    </Link>
+                    .
+                    <Link href="/tutorials/rules#PP" style={{ marginLeft: '.5rem' }}>
+                        Privacy Policy
+                    </Link>
+                </p>
+                <IconCropped width={24} height={24} className={styles.logo} />
+            </footer>
+        </div>
+    );
 };
 
-export default Home;
+export default HomeClient;

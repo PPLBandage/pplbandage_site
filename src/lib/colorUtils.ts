bbbed520095bde7940ade53306cd2ba752f13229
Andcool-Systems <drolll2007@gmail.com>
2025-08-19 12:43:39 +0300

    feat: Added Me Card text color adjusting

--- src/lib/colorUtils.ts
+++ src/lib/colorUtils.ts
@@ -1,3 +1,5 @@
+import AsyncImage from './asyncImage';
+
 export const getRandomColor = () => {
     const letters = '0123456789ABCDEF';
     let color = '#';
@@ -14,4 +16,50 @@ export const componentToHex = (c: number) => {
 
 export const rgbToHex = (r: number, g: number, b: number) => {
     return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b);
+};
+
+export const getAverageColor = async (url: string) => {
+    const image = await AsyncImage(url);
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d')!;
+
+    canvas.width = image.naturalWidth;
+    canvas.height = image.naturalHeight;
+
+    ctx.drawImage(image, 0, 0);
+
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+
+    let r = 0,
+        g = 0,
+        b = 0;
+
+    for (let i = 0; i < data.length; i += 4) {
+        r += data[i]; // R
+        g += data[i + 1]; // G
+        b += data[i + 2]; // B
+    }
+
+    const pixelCount = data.length / 4;
+    r = Math.round(r / pixelCount);
+    g = Math.round(g / pixelCount);
+    b = Math.round(b / pixelCount);
+
+    return { r, g, b };
+};
+export const hexToRgb = (hex: string) => {
+    if (hex.startsWith('#')) {
+        hex = hex.slice(1);
+    }
+
+    if (hex.length !== 6) {
+        throw new Error('Invalid hex color format');
+    }
+
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
+
+    return { r, g, b };
 };

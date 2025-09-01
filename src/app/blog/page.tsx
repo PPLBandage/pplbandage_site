d9dfa2bbd6a328fb276818f7edb7a804d933e273
Andcool-Systems <drolll2007@gmail.com>
2025-09-02 00:31:15 +0300

    fix: Improve article sorting and rendering logic in Blog component

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,35 +1,40 @@
 import { ArticleServer } from '@/components/blog/Article.server';
 import { getLayoutContents } from '@/lib/blog/loader';
 import style from '@/styles/blog/main.module.css';
+import { LayoutContent } from '@/types/blog';
 
 const Blog = async () => {
     const articles = await getLayoutContents();
     const sorted_articles = Object.entries(articles)
         .reverse()
-        .sort((a, b) => {
-            if (a[1].pinned && !b[1].pinned) return -1;
-            if (!a[1].pinned && b[1].pinned) return 1;
-
-            return (
+        .sort(
+            (a, b) =>
                 new Date(b[1].created).getTime() - new Date(a[1].created).getTime()
-            );
-        });
+        );
+
+    const pinned = sorted_articles.filter(i => i[1].pinned);
+    const regular = sorted_articles.filter(i => !i[1].pinned);
+
+    const renderArticles = (data: [string, LayoutContent][]) => {
+        return data.map(([slug, article], id) => (
+            <ArticleServer
+                key={id}
+                id={id}
+                article={article}
+                href={`/blog/${slug}`}
+            />
+        ));
+    };
 
     return (
         <main className={style.main}>
             <h2 style={{ marginTop: 0, marginBottom: '2rem' }}>
                 Последние новости проекта PPLBandage
             </h2>
-            <div className={style.grid}>
-                {sorted_articles.map(([slug, article], id) => (
-                    <ArticleServer
-                        key={id}
-                        id={id}
-                        article={article}
-                        href={`/blog/${slug}`}
-                    />
-                ))}
-            </div>
+            <h3>Закреплённые</h3>
+            <div className={style.grid}>{renderArticles(pinned)}</div>
+            <hr style={{ marginTop: '1.5rem', marginBottom: '1.5rem' }} />
+            <div className={style.grid}>{renderArticles(regular)}</div>
         </main>
     );
 };

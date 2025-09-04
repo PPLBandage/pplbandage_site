5b2dac45e5138028d26fbee149df97eb55217ea1
Andcool-Systems <drolll2007@gmail.com>
2025-09-04 22:00:11 +0300

    fix: Add 'controls' property to video tag in allowedProps for enhanced media support

--- src/lib/blog/loader.ts
+++ src/lib/blog/loader.ts
@@ -21,7 +21,7 @@ const commonProps = ['key', 'children'];
 const allowedProps: { [key: string]: string[] } = {
     a: ['href', 'target'].concat(commonProps),
     img: ['src'].concat(commonProps),
-    video: ['src', 'type', 'width'].concat(commonProps),
+    video: ['src', 'type', 'width', 'controls'].concat(commonProps),
     Emote: ['name'].concat(commonProps)
 };
 

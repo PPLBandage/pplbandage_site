b6fec0ef6b6d344fae8af381c80cbfa66a449635
Andcool-Systems <drolll2007@gmail.com>
2025-10-08 23:04:21 +0300

    feat: refactor data fetching to use SWR for improved performance and readability

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -6,15 +6,15 @@ import { SimpleGrid } from '@/components/workshop/AdaptiveGrid';
 import { renderSkin } from '@/lib/SkinCardRender';
 import { CreateCard } from '@/components/workshop/Card';
 import { getMeWorks } from '@/lib/api/user';
+import useSWR from 'swr';
 
 const Main = () => {
+    const { data } = useSWR('me-works', async () => await getMeWorks());
     const [cards, setCards] = useState<JSX.Element[] | null>(null);
 
     useEffect(() => {
-        getMeWorks()
-            .then(data => renderSkin(data.reverse(), styles).then(setCards))
-            .catch(console.error);
-    }, []);
+        if (data) renderSkin(data.reverse(), styles).then(setCards);
+    }, [data]);
 
     if (cards === null) return null;
     if (cards.length === 0) return <NoBandages />;
--- src/app/me/stars/page.tsx
+++ src/app/me/stars/page.tsx
@@ -4,32 +4,31 @@ import React, { JSX } from 'react';
 import { useEffect, useState } from 'react';
 import style_sidebar from '@/styles/me/sidebar.module.css';
 import styles_me from '@/styles/me/me.module.css';
-import { Bandage } from '@/types/global.d';
 import { SimpleGrid } from '@/components/workshop/AdaptiveGrid';
 import { renderSkin } from '@/lib/SkinCardRender';
 import { Placeholder } from '@/components/me/Placeholder';
 import { getMeStars } from '@/lib/api/user';
 import { BottomPaginator } from '@/components/workshop/BottomPaginator';
+import useSWR from 'swr';
 
 const Main = () => {
     const [page, setPage] = useState<number>(0);
     const [totalCount, setTotalCount] = useState<number>(0);
     const [elements, setElements] = useState<JSX.Element[] | null>(null);
 
-    const render_skins = (data: Bandage[]) => {
-        renderSkin(data, styles_me).then(setElements);
-    };
+    const { data } = useSWR(
+        `me-stars:${page}`,
+        async () => await getMeStars({ page, take: 12 })
+    );
 
     useEffect(() => {
-        getMeStars({ page, take: 12 })
-            .then(response => {
-                render_skins(response.data);
-                setTotalCount(response.totalCount);
-            })
-            .catch(console.error);
+        if (data) {
+            renderSkin(data.data, styles_me).then(setElements);
+            setTotalCount(data.totalCount);
+        }
 
         window.scrollTo({ top: 0, behavior: 'smooth' });
-    }, [page]);
+    }, [data]);
 
     if (elements === null) return null;
     if (elements.length === 0) return <Placeholder />;

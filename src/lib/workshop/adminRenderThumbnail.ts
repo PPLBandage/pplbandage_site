cd6e8b036c56e1cdb51d4532026051e3d93307a4
Andcool-Systems <drolll2007@gmail.com>
2025-12-20 22:19:11 +0300

    feat: enhance thumbnail rendering by utilizing AsyncImage and optimizing canvas operations

--- src/lib/workshop/adminRenderThumbnail.ts
+++ src/lib/workshop/adminRenderThumbnail.ts
@@ -3,13 +3,13 @@ import useAccess from '../useAccess';
 import { hasThumbnail, uploadThumbnail } from '../api/workshop';
 import { renderQueue } from './RenderingQueue';
 import { Bandage } from '@/types/global';
+import AsyncImage from '../asyncImage';
 
 const useRenderThumbnail = (id: Bandage) => {
     const access = useAccess();
 
     useEffect(() => {
-        const superAdmin = access.includes(5);
-        if (!superAdmin && !access.includes(8)) {
+        if (!access.includes(5) && !access.includes(8)) {
             return;
         }
 
@@ -18,6 +18,7 @@ const useRenderThumbnail = (id: Bandage) => {
             if (has_thumbnail) return;
 
             console.info('Thumbnail not found! Rendering...');
+
             const { result } = renderQueue.enqueue({
                 b64: id.base64,
                 flags: id.flags,
@@ -25,7 +26,30 @@ const useRenderThumbnail = (id: Bandage) => {
             });
 
             const rendered_result = await result;
-            const blob = await (await fetch(rendered_result)).blob();
+            const renderImg = await AsyncImage(rendered_result);
+            const backgroundImg = await AsyncImage('/static/background.png');
+
+            const canvas = document.createElement('canvas');
+            canvas.width = backgroundImg.width;
+            canvas.height = backgroundImg.height;
+            const ctx = canvas.getContext('2d')!;
+
+            ctx.drawImage(backgroundImg, 0, 0);
+
+            const x = (canvas.width - renderImg.width) / 2;
+            const y = (canvas.height - renderImg.height) / 2;
+            ctx.drawImage(renderImg, x, y);
+
+            const blob: Blob = await new Promise((resolve, reject) =>
+                canvas.toBlob(blob => {
+                    if (blob) {
+                        resolve(blob);
+                    } else {
+                        reject(new Error('Failed to create blob'));
+                    }
+                }, 'image/png')
+            );
+
             const formData = new FormData();
             formData.append('file', blob, 'image.png');
             await uploadThumbnail(id.external_id, formData);

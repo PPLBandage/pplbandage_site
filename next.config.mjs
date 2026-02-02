aed35ffdb8843ea551685d0770e39a903d70ac08
Andcool-Systems <drolll2007@gmail.com>
2026-02-02 20:45:26 +0300

    fix(next.config): correct formatting and indentation in configuration file

--- next.config.mjs
+++ next.config.mjs
@@ -2,87 +2,87 @@
 import bundleAnalyzer from '@next/bundle-analyzer';
 
 const withBundleAnalyzer = bundleAnalyzer({
-	enabled: process.env.ANALYZE === 'true',
+    enabled: process.env.ANALYZE === 'true'
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	allowedDevOrigins: ['192.168.0.53'],
-	reactStrictMode: false,
-	images: {
-		dangerouslyAllowLocalIP: true,
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "192.168.0.53",
-				port: "",
-				pathname: "/**",
-			},
-			{
-				protocol: "https",
-				hostname: "pplbandage.ru",
-				port: "",
-				pathname: "/**",
-			},
-			{
-				protocol: "https",
-				hostname: "pplb.ru",
-				port: "",
-				pathname: "/**",
-			},
-			{
-				protocol: "https",
-				hostname: "dev2.andcool.ru",
-				port: "",
-				pathname: "/**",
-			},
-			{
-				protocol: "https",
-				hostname: "github.com",
-				port: "",
-				pathname: "/**",
-			},
-		],
-	},
-	webpack(config) {
-		config.cache = false;
-		config.module.rules.push({
-			test: /\.svg$/,
-			use: ['@svgr/webpack'],
-		});
+    allowedDevOrigins: ['192.168.0.53'],
+    reactStrictMode: false,
+    images: {
+        dangerouslyAllowLocalIP: true,
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: '192.168.0.53',
+                port: '',
+                pathname: '/**'
+            },
+            {
+                protocol: 'https',
+                hostname: 'pplbandage.ru',
+                port: '',
+                pathname: '/**'
+            },
+            {
+                protocol: 'https',
+                hostname: 'pplb.ru',
+                port: '',
+                pathname: '/**'
+            },
+            {
+                protocol: 'https',
+                hostname: 'dev.andcool.ru',
+                port: '',
+                pathname: '/**'
+            },
+            {
+                protocol: 'https',
+                hostname: 'github.com',
+                port: '',
+                pathname: '/**'
+            }
+        ]
+    },
+    webpack(config) {
+        config.cache = false;
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack']
+        });
 
-		return config;
-	},
-	turbopack: {
-		rules: {
-			'*.svg': {
-				loaders: ['@svgr/webpack'],
-				as: '*.js',
-			},
-		}
-	},
-	async headers() {
-		return [
-			{
-				source: '/static/:path*',
-				headers: [
-					{
-						key: 'Cache-Control',
-						value: 'public, max-age=86400, immutable'
-					}
-				]
-			},
-			{
-				source: '/static/:all*',
-				headers: [
-					{
-						key: 'Cache-Control',
-						value: 'public, max-age=86400'
-					}
-				]
-			}
-		]
-	}
+        return config;
+    },
+    turbopack: {
+        rules: {
+            '*.svg': {
+                loaders: ['@svgr/webpack'],
+                as: '*.js'
+            }
+        }
+    },
+    async headers() {
+        return [
+            {
+                source: '/static/:path*',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=86400, immutable'
+                    }
+                ]
+            },
+            {
+                source: '/static/:all*',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=86400'
+                    }
+                ]
+            }
+        ];
+    }
 };
 
 export default withBundleAnalyzer(nextConfig);

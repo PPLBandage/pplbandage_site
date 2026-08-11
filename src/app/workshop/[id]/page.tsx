5f85657ea750e5d90637ce7595fcb4dbd687c905
Andcool-Systems <drolll2007@gmail.com>
2026-08-11 23:39:01 +0300

    test

--- src/app/workshop/[id]/page.tsx
+++ src/app/workshop/[id]/page.tsx
@@ -23,6 +23,8 @@ export const generateMetadata = async ({
     );
 
     const data = meta.data.data as Interfaces.Bandage;
+
+    console.log(data);
     if (!data) return {};
     const stars = numbersTxt(data.stars_count, ['звезда', 'звезды', 'звёзд']);
     return {
